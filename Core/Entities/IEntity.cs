4be27b7d7600a1e8573f57ab0df37150dd6c6104
Ayberk Baydar <abaydar@bimser.com.tr>
2021-10-10 15:54:17 +0300

    10.10.2021

--- Core/Entities/IEntity.cs
+++ Core/Entities/IEntity.cs
@@ -1,4 +1,8 @@
-﻿namespace Entities.Abstract
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
 {
     //IEntity implement eden class bir veritabanı tablosudur.
     public interface IEntity
