6b88d214c20636677cd7fc5c6f77e94cb0607849
Ayberk Baydar <abaydar@bimser.com.tr>
2021-10-17 22:59:30 +0300

    17.10.2021

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+
+        public Result(bool success, string message):this(success)//otomatik olarak tek parametreli ctor'u da çalıştıracak.
+        {
+            //get=>readonly (ctor içinde set işlemi yapılabilir.)
+            Message = message;
+        }
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
+
